d4360829c41a6d2827f22ec21c9a2e90e8be0947
settyraghav-hue <setty.raghav@gmail.com>
2026-04-18 17:53:01 -0500

    Updating Sample.js

--- Sample.js
+++ Sample.js
@@ -2,5 +2,6 @@ console.log("~~~~~~~~~~");
 console.log("Hello World");
 console.log("Welcome to GitHub!");
 console.log("This is a sample JavaScript file.");
-console.log("Added this line today")
+console.log("Added this line today");
+console.log("This is a demo to checkout and add lines to the file");
 console.log("~~~~~~~~~~");
